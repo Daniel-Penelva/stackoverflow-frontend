f3229c7e368036901397fc21d29935f58b66a66d
Daniel Penelva <d4n.andrade@gmail.com>
2024-11-25 20:55:14 -0300

    feat: add interface de cadastro de usuário no SignupComponent (.html) + add Customização SCSS + add os imports do angular material
    Definindo interface de cadastro de usuário utilizando componentes estilizados do Angular Material. Ele combina elementos como mat-card, mat-form-field, e matInput para criar um formulário simples e visualmente atrativo.
    A <div class="signup-container"> é o container externo que serve para organizar o layout do formulário.

--- src/app/angular-material/app-material.module.ts
+++ src/app/angular-material/app-material.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [],
@@ -12,7 +15,10 @@ import { MatButtonModule } from '@angular/material/button';
     CommonModule,
     MatSlideToggleModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class AppMaterialModule { }
