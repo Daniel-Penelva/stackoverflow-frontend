e9d70fd0585176876430f4d1056b7d1cbc7f167b
Daniel Penelva <d4n.andrade@gmail.com>
2024-12-03 00:20:08 -0300

    feat: alterando o método login para armazenar o token de autenticação no localStorage + add métodos saveUser e saveToken no storage.service para manipular o armazenamento do token no localStorage
    O método login é responsavel por  autenticar o usuário enviando suas credenciais (e-mail e senha) para o endpoint de autenticação do backend. Ele processa a resposta para salvar os dados do usuário e o token JWT no armazenamento local. (1) envio da solicitação HTTP, utilizando o método post do HttpClient para enviar o objeto loginRequest (contendo e-mail e senha) para o endpoint de autenticação (authentication). (2) observação da resposta completa, a opção { observe: 'response' } permite acessar os cabeçalhos e o corpo da resposta. (3) processamento da resposta, usando o operador map do RxJS para salvar o corpo da resposta (usuário) no armazenamento local, para recuperar o cabeçalho de autenticação (Authorization) e extrair o token JWT e para salvar o token no armazenamento local (localStorage). (4) tratamento de erros com o operador catchError captura erros na solicitação. (5) regsitro de logs para o método log que é usado para registrar mensagens úteis no console.
    
    O serviço do StorageService é responsável por manipular o armazenamento local do navegador, salvando e removendo informações como o token de autenticação e os dados do usuário. O método "saveUser" remove qualquer valor previamente armazenado no localStorage sob a chave c_user e salva um novo objeto de usuário, convertido para string JSON. O método "saveToken" remove qualquer valor previamente armazenado na chave c_token e salva o novo token JWT diretamente como string.

--- src/app/auth-services/auth-service/auth.service.ts
+++ src/app/auth-services/auth-service/auth.service.ts
@@ -1,26 +1,51 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable, throwError } from 'rxjs';
+import { catchError, map, Observable, tap, throwError } from 'rxjs';
 import { SignupRequest } from '../../model/SignupRequest';
 import { LoginRequest } from '../../model/LoginRequest';
+import { StorageService } from '../storage-service/storage.service';
 
 const BASIC_URL = "http://localhost:8080/";
+
+export const AUTH_HEADER = "authorization";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private storage: StorageService) { }
 
   // Método responsável por enviar os dados de registro do usuário para o endpoint sign-up.
   signup(signupRequest: SignupRequest): Observable<any> {
     return this.http.post(BASIC_URL + "sign-up", signupRequest).pipe(catchError(this.handleError));
   }
 
-  // 
+  // Método responsável por enviar os dados do login (email e password) para o endpoint authentication
   login(loginRequest: LoginRequest): Observable<any> {
-    return this.http.post(BASIC_URL + "authentication", loginRequest).pipe(catchError(this.handleError));
+    return this.http.post(BASIC_URL + "authentication", loginRequest, { observe: 'response' }).pipe(
+      tap(__ => this.log("User Authentication")), 
+      map((res: HttpResponse<any>) => {
+        this.storage.saveUser(res.body);
+  
+        // Verifica se o cabeçalho está presente antes de usar
+        const authHeader = res.headers.get(AUTH_HEADER);
+        if (authHeader) {
+          const tokenLength = authHeader.length;
+          const bearerToken = authHeader.substring(7, tokenLength);
+          this.storage.saveToken(bearerToken);
+        } else {
+          console.error(`Cabeçalho ${AUTH_HEADER} não encontrado na resposta HTTP.`);
+        }
+  
+        return res;
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  log(message: string): void {
+    console.log("User Auth Service: " + message);
   }
 
   // Método de tratamento de erros que captura falhas na comunicação com o servidor.
