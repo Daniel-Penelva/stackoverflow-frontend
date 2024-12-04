0fbc4390e1752664a7bb443947baa64950d7eb3e
Daniel Penelva <d4n.andrade@gmail.com>
2024-12-03 22:42:50 -0300

    feat: (alterando status de login do usuário - add Logged_In)  add logica de autenticação e navegação do usuário no AppComponent + add novas funcionalidade do token no StorageService
    No AppComponent .ts:
    Add a variavel isUserLoggedIn que controla se o layout deve exibir opções de login/cadastro ou de painel/logout.
    Add atualização do Status do Usuário (updateUserLoggedInStatus), aqui, usa o método StorageService.isUserLoggedIn() para verificar se há um token armazenado no navegador. Ele é chamado no ngOnInit e sempre que ocorre uma navegação (NavigationEnd), garantindo que a interface esteja sempre sincronizada com o estado de autenticação.
    Add logout que remove o token e o usuário armazenados no localStorage usando StorageService.logout() e redireciona o usuário para a página de login.
    
    AppComponent .html:
    Add um layout condicional, aqui usa o Angular *ngIf para alternar entre os layouts: (1) Se não logado: Exibe botões de login e cadastro. (2) Se logado: Exibe botões de painel e logout. Na navegação o routerLink e routerLinkActive gerenciam a navegação e destacam os botões da página ativa.
    
    StorageService:
    O método getToken recupera o token do armazenamento local.
    O método isUserLoggedIn verifica se o token está presente no armazenamento local e retorna false se o token não existir.
    O método logout remove o token e os dados do usuário do armazenamento local, efetivamente deslogando o usuário.

--- src/app/auth-services/storage-service/storage.service.ts
+++ src/app/auth-services/storage-service/storage.service.ts
@@ -10,14 +10,35 @@ export class StorageService {
 
   constructor() { }
 
+  // remove qualquer valor previamente armazenado no localStorage sob a chave c_user e salva um novo objeto de usuário, convertido para string JSON.
   public saveUser(user: any) {
     window.localStorage.removeItem(USER);
     window.localStorage.setItem(USER, JSON.stringify(user));
   }
 
+  // remove qualquer valor previamente armazenado na chave c_token e salva o novo token JWT diretamente como string.
   public saveToken(token: string) {
     window.localStorage.removeItem(TOKEN);
     window.localStorage.setItem(TOKEN, token);
+  }
+
+  // recupera o token do armazenamento local.
+  static getToken(): any {
+    return localStorage.getItem(TOKEN);
+  }
+
+  // verifica se o token está presente no armazenamento local e retorna false se o token não existir.
+  static isUserLoggedIn() {
+    if(this.getToken() === null) {
+      return false;
+    }
+    return true;
+  }
+
+  // remove o token e os dados do usuário do armazenamento local, efetivamente deslogando o usuário.
+  static logout() {
+    window.localStorage.removeItem(TOKEN);
+    window.localStorage.removeItem(USER);
   }
 
 }
