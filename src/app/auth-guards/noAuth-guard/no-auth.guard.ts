792941e7ebc4d070a87160cb1a6b8760e2ce1ab3
Daniel Penelva <d4n.andrade@gmail.com>
2024-12-09 22:01:56 -0300

    feat: criando guard para no-auth e user
    Criando o guard no-auth e user - entrando com os comandos no terminal:
    ng g g auth-guards/noAuth-guard/no-auth
    ng g g auth-guards/user-guard/user
    Vou optar pela opção da guard "CanActivate".
    
    Vale ressaltar que guards (ou guardiões) são interfaces que permitem proteger ou restringir o acesso a rotas específicas da aplicação. Eles são usados no contexto do Router para determinar se uma determinada rota pode ou não ser ativada, desativada, carregada ou acessada em determinadas condições. O angular fornece quatro tipos de Guards, no caso, eu optei pelo "CanActivate" que verifica se uma rota pode ser ativada. Útil para verificar autenticação ou autorização antes de carregar um componente.

---
+++ src/app/auth-guards/noAuth-guard/no-auth.guard.ts
@@ -0,0 +1,14 @@
+import { CanActivateFn } from '@angular/router';
+
+export const noAuthGuard: CanActivateFn = (route, state) => {
+  return true;
+};
+
+
+/**
+ * Vale ressaltar que guards (ou guardiões) são interfaces que permitem proteger ou restringir o acesso a rotas específicas da aplicação. Eles 
+ * são usados no contexto do Router para determinar se uma determinada rota pode ou não ser ativada, desativada, carregada ou acessada em 
+ * determinadas condições. 
+ * O angular fornece quatro tipos de Guards, no caso, eu optei pelo "CanActivate" que verifica se uma rota pode ser ativada. Útil para verificar 
+ * autenticação ou autorização antes de carregar um componente.
+*/
